a41f5efc00d56f57c1e494223137addf999c7b20
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-12-06 14:07:39 +0530

    Initial commit

---
+++ component/layout.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <header>World Ranks</header>
+        <p>main</p>
+        <p>footer</p>
+      </div>
+    );
+  }
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from "../component/layout";
+class IndexPage extends React.Component {
+  
+  re
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
